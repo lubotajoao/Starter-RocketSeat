b59ac465e4bfa105b91b09643c4531210bbc7b67
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-10 13:15:56 -0300

    JS Assíncono: Utilizando Axios

--- curso-JavaScript/JS-Assincrono-AJAX/main.js
+++ curso-JavaScript/JS-Assincrono-AJAX/main.js
@@ -1,28 +1,9 @@
 /**
- * Promises
+ * Fazendo requisição usando API do Axios
  */
 
-var minhaPromise = function () {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://api.github.com/users/lubotajoao");
-    xhr.send(null);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject("Erro na requição");
-        }
-        console.log(JSON.parse(xhr.responseText));
-      }
-    };
-  });
-};
-
-minhaPromise()
+axios
+  .get("https://api.github.com/users/lubotajoao")
   .then(function (response) {
     console.log(response);
   })
