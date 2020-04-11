6d0ef73992b710eb5c9d3ca175fee56265b2de6e
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-11 20:37:57 -0300

    Async/Await

--- curso-JavaScript-ES6/src/main.js
+++ curso-JavaScript-ES6/src/main.js
@@ -1,1 +1,43 @@
-alert('Oii');
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("OK");
+    }, 2000);
+  });
+
+// minhaPromise()
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {});
+
+// async function executaPromise() {
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+
+//   // Sem utiilzar async/await ficaria em cascata da seguinte forma
+//   // minhaPromise().then((response) => {
+//   //   console.log(response);
+
+//   //   minhaPromise().then((response) => {
+//   //     console.log(response);
+
+//   //     minhaPromise().then((response) => {
+//   //       console.log(response);
+//   //     });
+//   //   });
+//   // });
+
+//   // Tudo isto e' convertido nestas tres linhas acima menos verbosas
+// }
+
+// Async/Await são utilizadas também numa arrow function
+
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+};
+
+executaPromise();
