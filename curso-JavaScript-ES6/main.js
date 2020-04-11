961ec2696e1c76973b49b71e5d6d89e0793dd510
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-11 01:52:57 -0300

    Conceitos: RESET/SPREAD

--- curso-JavaScript-ES6/main.js
+++ curso-JavaScript-ES6/main.js
@@ -1,40 +1,57 @@
 /**
- * Desestruturacao
+ * REST -> resto, restante, o que sobra
  */
 
-const usuario = {
-  nome: "Lubota",
-  idade: 30,
-  endereco: {
-    cidade: "Sao Paulo",
-    estado: "SP",
-  },
-};
+// const usuario = {
+//   nome: "Lubota",
+//   idade: 23,
+//   empresa: "Lenght",
+// };
 
-// const nome = usuario.nome;
-// const idade = usuario.idade;
-// const cidade = usuario.endereco.cidade;
-
-/**
- * Utilizando o conceito de desestruturacao abaixo, o que reflete as tres linhas
- * acima, coletando cada informacao desejada
- */
-// const {
-//   nome,
-//   idade,
-//   endereco: { cidade },
-// } = usuario;
+// const { nome, ...resto } = usuario;
 
 // console.log(nome);
-// console.log(idade);
-// console.log(cidade);
+// console.log(resto);
 
-// function mostraNome(usuario) {
-//   console.log(usuario.nome);
+// Array
+// const arr = [1, 2, 3, 4];
+
+// const [a, b, ...c] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Parametros de funcoes
+// Exemplo Funcao Soma
+// function soma(a, b, c, ..., n){
+//   return a + b + c ... + n;
 // }
 
-function mostraNome({ nome, idade }) {
-  console.log(nome, idade);
+// console.log(soma(1,3,4...,n))
+
+function soma(...params) {
+  return params.reduce((total, next) => total + next);
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 3, 4, 9));
+
+/**
+ * SPREAD -> propaga os dados para outras estruturas de dados
+ */
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+// const arr3 = [...arr1, ...arr2];
+
+// console.log(arr3);
+
+const usuario1 = {
+  nome: "Lubota",
+  idade: 30,
+  empresa: "Lenght",
+};
+
+const usuario2 = { ...usuario1, nome: "Joao" };
+
+console.log(usuario2);
