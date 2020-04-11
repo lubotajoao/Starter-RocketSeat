3826c61fc4b74e9916c2c5599bb4e6fac918f0e7
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-11 20:58:37 -0300

    Async/Await: Configurando Axios

--- curso-JavaScript-ES6/src/main.js
+++ curso-JavaScript-ES6/src/main.js
@@ -1,43 +1,18 @@
-const minhaPromise = () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("OK");
-    }, 2000);
-  });
+import axios from "axios";
 
-// minhaPromise()
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((err) => {});
+class Api {
+  static async getUserInfo(userName) {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${userName}`
+      );
 
-// async function executaPromise() {
-//   console.log(await minhaPromise());
-//   console.log(await minhaPromise());
-//   console.log(await minhaPromise());
+      console.log(response);
+    } catch {
+      console.warn("Erro na API");
+    }
+  }
+}
 
-//   // Sem utiilzar async/await ficaria em cascata da seguinte forma
-//   // minhaPromise().then((response) => {
-//   //   console.log(response);
-
-//   //   minhaPromise().then((response) => {
-//   //     console.log(response);
-
-//   //     minhaPromise().then((response) => {
-//   //       console.log(response);
-//   //     });
-//   //   });
-//   // });
-
-//   // Tudo isto e' convertido nestas tres linhas acima menos verbosas
-// }
-
-// Async/Await são utilizadas também numa arrow function
-
-const executaPromise = async () => {
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-};
-
-executaPromise();
+Api.getUserInfo("diego3g");
+Api.getUserInfo("diego3gfa");
