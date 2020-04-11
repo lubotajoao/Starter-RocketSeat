ffe4358b726085c9b0b6fec5e0f5ac5409d4d449
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-11 01:14:27 -0300

    Conceitos: Arrow Functions

--- curso-JavaScript-ES6/bundle.js
+++ curso-JavaScript-ES6/bundle.js
@@ -1,24 +1,10 @@
 "use strict";
 
 var arr = [1, 2, 3, 4, 5, 7, 18, 71, 72]; // const newArr = arr.map(function (item) {
-//   return item * 2;
+//   return item * 3;
+// });
 
-var newArr = arr.map(function (item, index) {
-  return item + index;
+var newArr = arr.map(function (item) {
+  return item * 3;
 });
-console.log(newArr); // Reduce -> forma de consumir todo vetor e transformar em uma unica informacao/variavel/elemento
-
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum); // Filter ->
-
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter); // Finde ->
-
-var find = arr.find(function (item) {
-  return item === 7;
-});
-console.log(find);
+console.log(newArr);
--- curso-JavaScript-ES6/main.js
+++ curso-JavaScript-ES6/main.js
@@ -1,31 +1,9 @@
 const arr = [1, 2, 3, 4, 5, 7, 18, 71, 72];
 
 // const newArr = arr.map(function (item) {
-//   return item * 2;
+//   return item * 3;
+// });
 
-const newArr = arr.map(function (item, index) {
-  return item + index;
-});
+const newArr = arr.map((item) => item * 3);
 
 console.log(newArr);
-
-// Reduce -> forma de consumir todo vetor e transformar em uma unica informacao/variavel/elemento
-const sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-
-console.log(sum);
-
-// Filter ->
-const filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-
-console.log(filter);
-
-// Finde ->
-const find = arr.find(function (item) {
-  return item === 7;
-});
-
-console.log(find);
