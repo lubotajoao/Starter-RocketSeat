4f0b40a0e63d4714b3bb5b08322c781f306d5cd3
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-11 00:48:23 -0300

    Conceitos: Operacoes em array (métodos)

--- curso-JavaScript-ES6/bundle.js
+++ curso-JavaScript-ES6/bundle.js
@@ -1,7 +1,24 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5, 7, 18];
-var newArr = arr.map(function (item) {
-  return item * 2;
+var arr = [1, 2, 3, 4, 5, 7, 18, 71, 72]; // const newArr = arr.map(function (item) {
+//   return item * 2;
+
+var newArr = arr.map(function (item, index) {
+  return item + index;
 });
-console.log(newArr);
+console.log(newArr); // Reduce -> forma de consumir todo vetor e transformar em uma unica informacao/variavel/elemento
+
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum); // Filter ->
+
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter); // Finde ->
+
+var find = arr.find(function (item) {
+  return item === 7;
+});
+console.log(find);
