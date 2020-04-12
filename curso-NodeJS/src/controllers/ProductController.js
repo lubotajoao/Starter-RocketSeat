67f5efd57d06441bd4800222235f7be4ce3551e2
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-12 19:17:40 -0300

    NodeJS: Reestruturacao de arquivos

---
+++ curso-NodeJS/src/controllers/ProductController.js
@@ -0,0 +1,15 @@
+/**
+ * O Controller lida com as operacoes que teremos em determinados models
+ */
+const mongoose = require("mongoose");
+
+const Product = mongoose.model("Product");
+
+module.exports = {
+  // Faz a listagem de todos os produtos que estiverem na nossa base
+  async index(request, response) {
+    const products = await Product.find();
+
+    return response.json(products);
+  },
+};
