eff97421a0797235e188ed2cd5b49e4687837fda
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-11 23:21:32 -0300

    Aplicação com ES6+: Render de repositorios na tela

--- curso-JavaScript-ES6/src/main.js
+++ curso-JavaScript-ES6/src/main.js
@@ -6,6 +6,7 @@ class App {
     this.repositories = [];
 
     this.formEl = document.getElementById("repo-form");
+    this.listEl = document.getElementById("repo-list");
 
     this.registerHandless();
   }
@@ -29,8 +30,39 @@ class App {
       avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
       html_url: "http://github.com/rocketseat",
     });
+    this.render();
+  }
 
-    console.log(this.repositories);
+  /**
+   * Apaga todo conteudo contido na tela e renderiza do Zero
+   * Vai percorrer o array e mostrar cada um dos repositorios do array
+   */
+  render() {
+    this.listEl.innerHTML = "";
+
+    // Criacao de todos os elementos que serao exibidos
+    this.repositories.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
