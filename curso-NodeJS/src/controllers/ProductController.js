7d3bdd6cc8260fe52f07e95ebdf6a5f2dc2becd6
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-12 20:21:07 -0300

    NodeJS: CRUD (Create, Read, Update & Delete)

--- curso-NodeJS/src/controllers/ProductController.js
+++ curso-NodeJS/src/controllers/ProductController.js
@@ -13,9 +13,31 @@ module.exports = {
     return response.json(products);
   },
 
+  async show(request, response) {
+    const product = await Product.findById(request.params.id);
+
+    return response.json(product);
+  },
+
   async store(request, response) {
     const product = await Product.create(request.body);
 
     return response.json(product);
+  },
+
+  async update(request, response) {
+    const product = await Product.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      { new: true }
+    );
+
+    return response.json(product);
+  },
+
+  async destroy(request, response) {
+    await Product.findByIdAndRemove(request.params.id);
+
+    return response.send();
   },
 };
--- curso-NodeJS/src/routes.js
+++ curso-NodeJS/src/routes.js
@@ -3,7 +3,10 @@ const routes = express.Router();
 
 const ProductController = require("./controllers/ProductController");
 
-routes.get("/products", ProductController.index);   // Rota de mostrar todos os produtos na tela.
-routes.post("/products", ProductController.store);  // Rota de persistencia de dados no DB
+routes.get("/products", ProductController.index); // Rota de mostrar todos os produtos na tela.
+routes.get("/products/:id", ProductController.show); // Rota que mostra um registro especifico
+routes.post("/products", ProductController.store); // Rota de persistencia de dados no DB
+routes.put("/products/:id", ProductController.update); // Rota de persistencia de dados no DB atualizacao
+routes.delete("/products/:id", ProductController.destroy); // Rota para deletar um registro no banco
 
 module.exports = routes;
