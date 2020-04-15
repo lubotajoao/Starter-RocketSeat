4833ff3230f804561d2296032dc03379cf4eb1c5
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-15 20:31:58 -0300

    Rev NodeJS: Conectando DataBase (Mongoose)

--- curso-NodeJS/node-api/server.js
+++ curso-NodeJS/node-api/server.js
@@ -1,6 +1,19 @@
 // Importando o express
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Inciando o App
 const app = express();
+
+// Iniciando o DataBase
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Primeira rota
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
 
 app.listen(3001);
