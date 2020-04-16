7f5874bcdc23367448f057ed496bd6177b7c7e71
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-16 17:43:31 -0300

    Rev NodeJS: Adicionando CORS

--- curso-NodeJS/node-api/server.js
+++ curso-NodeJS/node-api/server.js
@@ -1,13 +1,22 @@
-// Importando o express
+/**
+ * express      -> trata de rotas
+ * mongoose     -> para tratamento de banco de dados
+ * require-dir  -> manipula os caminhos de inclusao de arquivos de forma otimizada
+ * cors         -> permite que outras aplicacoes/software acessam o api (criado)
+ * (Cross-origin resource sharing)
+ *
+ */
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 
 /**
  * Inciando o App
  */
 const app = express();
 app.use(express.json()); // sinaliza o envio de dados no formato json (metodo post)
+app.use(cors());
 
 // Iniciando o DataBase
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
