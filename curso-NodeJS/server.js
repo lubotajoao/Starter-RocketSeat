53608189fcebdcf20a30ce81522fe5c726663b3e
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-16 22:26:29 -0300

    NodeJS: Criando a primeira rota utilizando o 'express'

--- curso-NodeJS/server.js
+++ curso-NodeJS/server.js
@@ -2,4 +2,22 @@ const express = require("express");
 
 const app = express();
 
+/**
+ * Request: simboliza toda requisição feita no servidor
+ * Request contém todas as informções desta requisição:
+ *  - Parâmetros;
+ *  - Corpo da requisição;
+ *  - Cabeçalho da requisição;
+ *  - Usuário que está fazendo a requisição;
+ *  - Autenticação (se o usuário está autenticado ou não)
+ *
+ * Response: tem a ver com a resposta dada para a requisição.
+ *
+ * O fluxo do backend é a gente fazer uma requisição e o servidor devolver uma resposta.
+ */
+
+app.get("/", (request, response) => {
+  response.send("Bonjour le monde!!!");
+});
+
 app.listen(3001);
