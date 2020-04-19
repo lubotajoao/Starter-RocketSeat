82da8d29e6b7f738aff665456d6e2057ed6d4036
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-18 22:17:02 -0300

    NodeJS: Conectando Database

--- curso-NodeJS/server.js
+++ curso-NodeJS/server.js
@@ -1,20 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Iniciando o App
 const app = express();
 
-/**
- * Request: simboliza toda requisição feita no servidor
- * Request contém todas as informções desta requisição:
- *  - Parâmetros;
- *  - Corpo da requisição;
- *  - Cabeçalho da requisição;
- *  - Usuário que está fazendo a requisição;
- *  - Autenticação (se o usuário está autenticado ou não)
- *
- * Response: tem a ver com a resposta dada para a requisição.
- *
- * O fluxo do backend é a gente fazer uma requisição e o servidor devolver uma resposta.
- */
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (request, response) => {
   response.send("Bonjour le monde !!!");
