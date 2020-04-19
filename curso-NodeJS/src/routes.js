54ffd338d07f6253fb03c391237efd08cf2a008a
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-19 00:02:50 -0300

    NodeJS: Criação de Registros

--- curso-NodeJS/src/routes.js
+++ curso-NodeJS/src/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const ProductController = require("./controllers/ProductController");
 
 routes.get("/products", ProductController.index);
+routes.post("/products", ProductController.store);
 
 // exportando o router para que seja utilizado em outros arquivos (com import)
 module.exports = routes;
