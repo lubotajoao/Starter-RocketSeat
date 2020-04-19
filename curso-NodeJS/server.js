90583d2ac4d286c45ec1d7d321f7b074e5567137
Lubota Ngola Joao <lubota4@gmail.com>
2020-04-18 22:40:53 -0300

    NodeJS: Criando Model do Product

--- curso-NodeJS/server.js
+++ curso-NodeJS/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express();
@@ -10,8 +11,18 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useUnifiedTopology: true,
 });
 
+// Efetivando a criação do model da aplicação
+requireDir("./src/models");
+const Product = mongoose.model("Product");
+
 app.get("/", (request, response) => {
-  response.send("Bonjour le monde !!!");
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "http://github.com/facebook/react-native",
+  });
+
+  return response.send("Bonjour le monde !!!");
 });
 
 app.listen(3001);
